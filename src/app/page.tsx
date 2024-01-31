f376240cf89bb8f19dc63c9303696524287d272c
pranit mane <pranitmane@pranits-MacBook-Pro.local>
2024-01-31 17:59:11 +0530

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
         <div className="absolute rounded-xl w-full h-full blur-3xl bg-gradient-to-r from-[#66B3FF]/30 to-[#FFB366]/30 top-0 left-0 z-[-10]">
         </div>
 
-        <h1 className="bg-gradient-to-r from-[#66B3FF] to-[#FFB366] text-transparent bg-clip-text text-6xl font-bold text-center">Nows</h1>
-        <h2 className="text-xl text-center text-neutral-200 font-semibold">Live in Present.</h2>
+        <h1 className="bg-gradient-to-r from-[#66B3FF] to-[#FFB366] text-transparent bg-clip-text text-6xl font-bold text-center">NOWS</h1>
+        <h2 className="text-2xl text-center text-neutral-200 font-semibold">Live in Present.</h2>
       </div>
       {/* <h1 className="text-lg text-neutral-400 tracking-widest typing">Coming Soon..</h1> */}
       <Message text="Coming Soon.." />
